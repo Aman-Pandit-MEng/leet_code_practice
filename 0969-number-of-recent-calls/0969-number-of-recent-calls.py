5dd84b4375614b144efa5947b747e54cca686d25
Aman <aman20p@gmail.com>
2025-12-23 15:01:31 +0100

    0969-NUMBER-OF-RECENT-CALLS Stats: Time: 8569 ms (5%), Space: 23.5 MB (8.85%) - LeetHub

---
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -0,0 +1,17 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.q = deque()
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+        return sum(1 for i in self.q if i >= t-3000 and i <= t)
+        #while self.q[0] < t - 3000:
+        #    self.q.popleft()
+        #return len(self.q)
+
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
